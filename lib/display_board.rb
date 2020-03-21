1b6154c98492e8e28c9fcd7ebd9dff3408fbcc49
Kevin Garcia <kg.gar91@gmail.com>
2020-03-21 22:22:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,11 +7,5 @@ def display_board(board)
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 end
 
-def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-end
+display_board(board)
